e683e37701f4d902b5ec771afb6e818b3fbb3857
Gauri Pawan <137494633+citruseye@users.noreply.github.com>
2024-07-08 16:47:24 +0530

    Update StoreContext.js

--- client/orderease/src/context/StoreContext.js
+++ client/orderease/src/context/StoreContext.js
@@ -7,7 +7,7 @@ export const StoreContext = createContext(null)
 const StoreContextProvider = (props) => {
 
     const [cartItems,setCartItems] = useState({});
-    const url = "http://localhost:4000"
+    const url = "https://digital-menu-zmtt.onrender.com"
     const [token,setToken] = useState("");
     const [foodData,setFoodList] = useState([])
 
